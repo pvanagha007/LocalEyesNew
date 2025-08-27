702f98f1c206ccf145b49ab0691358a1f586a367
Anshika <ansigupta2807@gmail.com>
2025-08-27 17:20:18 +0530

    chore: remove root-level front-end files, keep all under client/

--- src/pages/BecomeGuide.jsx
+++
@@ -1,7 +0,0 @@
-export default function BecomeGuide() {
-  return (
-    <div className="bg-brand text-white min-h-screen flex items-center justify-center font-inter">
-      <h1 className="text-4xl font-bold">Become a Guide Page</h1>
-    </div>
-  );
-}
